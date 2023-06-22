8cfc6ffb489efd68b382756673745e832fe7e519
NathanNavid <naaghajafari@gmail.com>
2023-06-22 05:47:51 -0700

    reg,login and logout are done and tested by postman

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8001;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+require('./config/mongoose.config');
+
+
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+require('./routes/user.route')(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
