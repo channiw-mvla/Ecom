5102c88707f144ee2d6f9d154a9ddf71fef9858f
yusufkarakaya <yusufkarakaya92@gmail.com>
2023-06-23 10:06:55 -0700

    added navbar, import axios

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   );
