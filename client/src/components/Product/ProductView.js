197de81d205af68f717312edc62fac0fdca98765
channiw-mvla <100023866@mvla.net>
2023-06-23 19:46:59 -0700

    shows products from database on home page

---
+++ client/src/components/Product/ProductView.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ProductView.css';
+import { useNavigate } from 'react-router-dom';
+
+const ProductView = ({image1,image2,name,price,sizes}) => {
+    const  navigate = useNavigate()
+  return (
+    <div className="product-view">
+      <img src={image1} alt="Product" className="img-fluid" onMouseOver={e => e.target.src = image2} onMouseOut={ e => e.target.src = image1} onClick={()=>navigate("/products/"+name)}/>
+      <h3 className="product-title">{name}</h3>
+      <p className="product-price">Price: ${price.toFixed(2)}</p>
+      <select
+        className="form-select mb-3"
+      >
+        <option value="">Select Size</option>
+        {sizes.map((value,index)=>{
+           return <option key={index} value={value}>{value}</option>
+        })}
+      </select>
+      <button className='btn btn-primary w-100' onClick={()=>navigate("/cart")}>
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default ProductView;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
+import ProductView from '../components/Product/ProductView';
+import axios from 'axios'
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <h1>Home</h1>
-    </div>
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const getProducts = async () => {
+      await axios.get("http://localhost:8000/api/products/")  
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err))
+    }
+    getProducts()
+    setLoading(false)
+  }, [])
+  if(loading)
+    return <h1>Loading</h1>
+  else
+    return (
+      <div>
+        <Navbar />
+        {/* <h1>Home</h1> */}
+        <div class="container my-3 ">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {products.map((product,index) => {
+              return(
+                <div class="col-md-4">
+                    < ProductView key={index} image1={product.images[0]} image2={product.images[1]} name={product.name} price={product.price} sizes = {product.sizes}/>
+                  </div>
+                  );
+                  
+                })}
+          </div>
+        </div>
+      </div>
   );
 }
