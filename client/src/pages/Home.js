0d5b44fc7bcd19381ce62f2c88de962fddc18712
channiw-mvla <100023866@mvla.net>
2023-06-23 19:56:20 -0700

    added no products and removed loading text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,6 @@ import axios from 'axios'
 
 export default function Home() {
   const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
   useEffect(() => {
     const getProducts = async () => {
       await axios.get("http://localhost:8000/api/products/")  
@@ -13,27 +12,32 @@ export default function Home() {
       .catch(err => console.log(err))
     }
     getProducts()
-    setLoading(false)
   }, [])
-  if(loading)
-    return <h1>Loading</h1>
-  else
     return (
       <div>
         <Navbar />
         {/* <h1>Home</h1> */}
         <div class="container my-3 ">
-          <div className="row row-cols-1 row-cols-md-3 g-4">
-            {products.map((product,index) => {
-              return(
-                <div class="col-md-4">
-                    < ProductView key={index} image1={product.images[0]} image2={product.images[1]} name={product.name} price={product.price} sizes = {product.sizes}/>
-                  </div>
-                  );
-                  
-                })}
+          {products.length === 0 ?
+            (
+              <div className="d-flex justify-content-center">
+                <h1>No Products</h1>
+              </div>
+            ):
+            (
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                  {products.map((product,index) => {
+                    return(
+                      <div class="col-md-4">
+                          < ProductView key={index} image1={product.images[0]} image2={product.images[1]} name={product.name} price={product.price} sizes = {product.sizes}/>
+                        </div>
+                        );
+                        
+                      })}
+                </div>
+            )
+          }
           </div>
         </div>
-      </div>
   );
 }
