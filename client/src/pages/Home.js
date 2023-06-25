379daec407a847edddc68b2c553ec1d0ba704efd
yusufkarakaya <yusufkarakaya92@gmail.com>
2023-06-25 13:01:58 -0700

    created productdetail page, needs design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,49 @@
 import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import ProductView from '../components/Product/ProductView';
-import axios from 'axios'
+import axios from 'axios';
 
 export default function Home() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     const getProducts = async () => {
-      await axios.get("http://localhost:8000/api/products/")  
-      .then(res => setProducts(res.data))
-      .catch(err => console.log(err))
-    }
-    getProducts()
-  }, [])
-    return (
-      <div>
-        <Navbar />
-        {/* <h1>Home</h1> */}
-        <div class="container my-3 ">
-          {products.length === 0 ?
-            (
-              <div className="d-flex justify-content-center">
-                <h1>No Products</h1>
-              </div>
-            ):
-            (
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                  {products.map((product,index) => {
-                    return(
-                      <div class="col-md-4">
-                          < ProductView key={index} image1={product.images[0]} image2={product.images[1]} name={product.name} price={product.price} sizes = {product.sizes}/>
-                        </div>
-                        );
-                        
-                      })}
-                </div>
-            )
-          }
+      await axios
+        .get('http://localhost:8000/api/products/')
+        .then((res) => setProducts(res.data))
+        .then((res) => console.log(products))
+        .catch((err) => console.log(err));
+    };
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      {/* <h1>Home</h1> */}
+      <div class='container my-3 '>
+        {products.length === 0 ? (
+          <div className='d-flex justify-content-center'>
+            <h1>No Products</h1>
           </div>
-        </div>
+        ) : (
+          <div className='row row-cols-1 row-cols-md-3 g-4'>
+            {products.map((product, index) => {
+              return (
+                <div class='col-md-4'>
+                  <ProductView
+                    key={index}
+                    _id={product._id}
+                    image1={product.images[0]}
+                    image2={product.images[1]}
+                    name={product.name}
+                    price={product.price}
+                    sizes={product.sizes}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
---
+++ client/src/pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,11 @@
+import Header from '../../components/Navbar/Navbar';
+export default function ProductDetail() {
+  return (
+    <>
+      <Header />
+      <h1>Product Detail</h1>
+      <div className='product-detail-left'></div>
+      <div className='product-detail-right'></div>
+    </>
+  );
+}
