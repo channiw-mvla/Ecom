6902a5777a9ceba7ed8ba6f74ce4a4749c5b831b
yusufkarakaya <yusufkarakaya92@gmail.com>
2023-06-25 13:14:58 -0700

    conflict fixed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,56 +14,30 @@ export default function Home() {
     };
     getProducts();
   }, []);
+
   return (
     <div>
       <Navbar />
       {/* <h1>Home</h1> */}
-      <div class='container my-3 '>
+      <div className='container my-3 '>
         {products.length === 0 ? (
           <div className='d-flex justify-content-center'>
             <h1>No Products</h1>
           </div>
         ) : (
           <div className='row row-cols-1 row-cols-md-3 g-4'>
-            {products.map((product, index) => {
-              return (
-                <div class='col-md-4'>
-                  <ProductView
-                    key={index}
-                    _id={product._id}
-                    image1={product.images[0]}
-                    image2={product.images[1]}
-                    name={product.name}
-                    price={product.price}
-                    sizes={product.sizes}
-                  />
-      await axios.get("http://localhost:8000/api/products/")  
-      .then(res => setProducts(res.data))
-      .catch(err => console.log(err))
-    }
-    getProducts()
-  }, [])
-    return (
-      <div>
-        <Navbar />
-        {/* <h1>Home</h1> */}
-        <div className="container my-3 ">
-          {products.length === 0 ?
-            (
-              <div className="d-flex justify-content-center">
-                <h1>No Products</h1>
+            {products.map((product) => (
+              <div className='col-md-4' key={product._id}>
+                <ProductView
+                  _id={product._id}
+                  image1={product.images[0]}
+                  image2={product.images[1]}
+                  name={product.name}
+                  price={product.price}
+                  sizes={product.sizes}
+                />
               </div>
-            ):
-            (
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                  {products.map((product) => (
-                      <div className="col-md-4" key={product._id}>
-                        < ProductView image1={product.images[0]} image2={product.images[1]} name={product.name} price={product.price} sizes = {product.sizes}/>
-                      </div>                        
-                  ))}
-                </div>
-              );
-            })}
+            ))}
           </div>
         )}
       </div>
