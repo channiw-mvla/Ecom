ce797eb9a472eae7e0eea2b9f1c67ff9071ab116
yusufkarakaya <yusufkarakaya92@gmail.com>
2023-06-28 22:32:54 -0700

    created bestseller page

--- client/src/pages/Bestsellers/Bestsellers.jsx
+++ client/src/pages/Bestsellers/Bestsellers.jsx
@@ -1,31 +1,40 @@
 import { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar/Navbar';
-import ProductView from '../components/Product/ProductView';
+import Navbar from '../../components/Navbar/Navbar';
+import ProductView from '../../components/Product/ProductView';
 import axios from 'axios';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     const getProducts = async () => {
-      await axios
-        .get('http://localhost:8000/api/products/')
-        .then((res) => setProducts(res.data))
-        .catch((err) => console.log(err));
+      try {
+        const response = await axios.get('http://localhost:8000/api/products/');
+        setProducts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+
     getProducts();
   }, []);
+
+  // Sort products by numOrders before rendering
+  const sortedProducts = [...products].sort(
+    (a, b) => b.numOrders - a.numOrders
+  );
 
   return (
     <div>
       <Navbar />
-      <div className='container my-3 '>
-        {products.length === 0 ? (
+      <div className='container my-3'>
+        {sortedProducts.length === 0 ? (
           <div className='d-flex justify-content-center'>
             <h1>No Products</h1>
           </div>
         ) : (
           <div className='row row-cols-1 row-cols-md-3 g-4'>
-            {products.map((product) => (
+            {sortedProducts.map((product) => (
               <div className='col-md-4' key={product._id}>
                 <ProductView
                   _id={product._id}
